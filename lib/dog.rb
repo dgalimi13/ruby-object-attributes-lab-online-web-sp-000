8b5b5da22cfe1fb3cd97a2e6f9d95e74f901939d
danny galimi <dannygalimi13@aim.com>
2020-09-02 22:39:56 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+def name=(input)
+  @this_dogs_name = input
+end
+
+def name 
+  @this_dogs_name
+end
+
+def breed=(input)
+  @this_dogs_breed = input
+end
+
+def breed 
+  @this_dogs_breed
+end
+
+end
